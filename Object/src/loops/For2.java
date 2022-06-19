5215dffde5ddfd288794f566ff880ca93e8ed9aa
Prasad <sanaka.prasad@outlook.com>
2022-06-19 12:17:27 +0530

    Added a comment

---
+++ Object/src/loops/For2.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class For2 {
+	// Java program to illustrate for loop
+	
+		public static void main(String args[])
+		{
+			// Writing a for loop
+			// to print Hello World 5 times
+			for (int i = 1; i <= 5; i++)
+				System.out.println("Hello World");
+		}
+	}
+
+
+
---
+++ Object/src/loops/For3.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class For3 {
+	public static void main(String args[])
+    {
+        int sum = 0;
+ 
+        // for loop begins
+        // and runs till x <= 20
+        for (int x = 1; x <= 20; x++) {
+            sum = sum + x;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
+
+
