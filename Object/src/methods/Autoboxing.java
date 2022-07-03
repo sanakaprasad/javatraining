27c6aec29d4a88d7c301e2f2dda044dc63f63b07
Prasad <sanaka.prasad@outlook.com>
2022-07-03 17:04:00 +0530

    Added a comment

---
+++ Object/src/methods/Autoboxing.java
@@ -0,0 +1,27 @@
+package methods;
+
+import java.util.ArrayList;
+
+class Autoboxing {
+	// Java program to demonstrate Autoboxing
+
+	
+	
+		public static void main(String[] args)
+		{
+			char ch = 'a';
+
+			// Autoboxing- primitive to Character object conversion
+			Character a = ch;
+
+			ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+			// Autoboxing because ArrayList stores only objects
+			arrayList.add(25);
+
+			// printing the values from object
+			System.out.println(arrayList.get(0));
+		
+	}
+
+}
---
+++ Object/src/operatorsex/Assignment.java
@@ -0,0 +1,26 @@
+package operatorsex;
+
+public class Assignment {
+	// Java code to illustrate "%="
+
+	
+
+	
+		public static void main(String[] args)
+		{
+
+			// Declaring variables
+			int num1 = 5, num2 = 3;
+
+			System.out.println("num1 = " + num1);
+			System.out.println("num2 = " + num2);
+
+			// Moduling & Assigning values
+			num1 %= num2;
+
+			// Displaying the assigned values
+			System.out.println("num1 = " + num1);
+		}
+	}
+
+
